a7a03e1b969c9ed1ec64827382b967c2d06f7f0e
Akshat Khandelwal <akshatk1001@gmail.com>
2024-08-23 00:04:34 -0700

    Day 1 Done

--- DetectRGB.py
+++ DetectRGB.py
@@ -1,1 +1,20 @@
-print("hello world")
+import cv2
+import os
+import matplotlib.pyplot as plt
+
+image = cv2.imread("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/Tomatoes.png")
+
+'''Showing image using OpenCV'''
+# cv2.imshow("Image", image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+'''Showing image using Matplotlib'''
+# plt.imshow(image)
+# plt.waitforbuttonpress()
+# plt.close('all')
+
+'''Changing the file type using OpenCV and deleting using OS'''
+# cv2.imwrite("convertTojpg.jpg", image)
+# print(os.listdir("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/"))
+# os.remove("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/convertTojpg.jpg")
