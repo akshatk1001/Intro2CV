0b16a6d9aa3fc1f43648c6e124fb8a0268da8845
Akshat Khandelwal <akshatk1001@gmail.com>
2024-08-29 21:11:00 -0700

    Added blurring

--- OpenCVBeginning.py
+++ OpenCVBeginning.py
@@ -3,8 +3,9 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-image = cv2.imread("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/Tomatoes.png")
-imageRGB = cv2.imread("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/RGB.png")
+imageTomatoes = cv2.imread("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/Tomatoes.png")
+imageRiana = cv2.imread("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/RianaHands.jpeg")
+imageMedianBlur = cv2.imread("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/MedianBlurLena.png")
 
 '''Showing image using OpenCV'''
 # cv2.imshow("Image", image)
@@ -21,4 +22,36 @@ imageRGB = cv2.imread("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2C
 # print(os.listdir("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/"))
 # os.remove("/Users/aneeshkhandelwal/Downloads/VSCode Projects/Intro2CV/convertTojpg.jpg")
 
+'''Adding images with different sizes/aspect ratios together'''
+# newRiana = cv2.resize(imageRiana, (imageTomatoes.shape[1], imageTomatoes.shape[0]), interpolation = cv2.INTER_AREA)
 
+# new_image = cv2.addWeighted(imageTomatoes, 0.5, newRiana, 0.5, 0)
+# subtract_image = cv2.subtract(imageTomatoes, newRiana)
+
+# cv2.imshow("pic", new_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows
+
+'''Blurring Images'''
+# cv2.imshow("Original Image", imageRiana)
+# cv2.imshow("Original Median Blur", imageMedianBlur)
+
+# kernels = [(15, 15)]
+# parameters = [(30, 45, 30)]
+
+# for (x, y) in kernels:
+#     blur = cv2.blur(imageRiana, (x, y)) #Just normal blur of image
+#     cv2.imshow("Normal Blur x:{}, y:{}".format(x, y), blur)
+
+#     blurred_image = cv2.GaussianBlur(imageRiana, (x, y), 0) #For images with some noise
+#     cv2.imshow("Gausian Blur x:{}, y:{}".format(x, y), blurred_image)
+
+# median_blur = cv2.medianBlur(imageMedianBlur, 7) #Used for when image has ALOT of noise (salt and pepper)
+# cv2.imshow("Median Blur 7", median_blur)
+
+# for(diameter, sigmaColor, sigmaSpace) in parameters:
+#     bilateral_blur = cv2.bilateralFilter(imageRiana, diameter, sigmaColor, sigmaSpace) # Reduce noise while still maintaining edges
+#     cv2.imshow("BilateralBlur d:{}, sC:{}, sWS:{}".format(diameter, sigmaColor, sigmaSpace), bilateral_blur)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
